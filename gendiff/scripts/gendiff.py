1b68d886fee0dc2adf9815f6755c293d164431df
Coverfog <Hain.Heither@gmail.com>
2025-09-06 22:32:56 +0300

    Update gendiff.py: added option -f/--format into help message

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,8 +6,13 @@ def main():
         prog='gendiff',
         description='Compares two configuration files and shows a difference.')
 
-    parser.add_argument('first_file', help='Path to the first file')
-    parser.add_argument('second_file', help='Path to the second file')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+
+    parser.add_argument('-f', '--format', help='set format of output', 
+        choices=['json', 'plain'],
+        default='plain')
+
     args = parser.parse_args()
 
 
