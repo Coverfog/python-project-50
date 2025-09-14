9e2e374a4dba059d934a8eba6788500da8c62c51
Coverfog <Hain.Heither@gmail.com>
2025-09-14 18:28:31 +0300

    Fix script gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,5 @@
 import argparse
+
 from gendiff.gendiff import generate_diff
 
 
@@ -18,6 +19,7 @@ def main():
 
     diff = generate_diff(args.first_file, args.second_file)
     print(diff)
+
 
 if __name__ == '__main__':
     main()
