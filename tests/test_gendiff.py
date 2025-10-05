149a7314b86f0819cfd4262899c2360913dd83dd
Coverfog <Hain.Heither@gmail.com>
2025-10-06 01:02:35 +0300

    Update: improved tests to match new logic

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -19,3 +19,23 @@ def test_plain_yaml():
         'tests/test_data/yaml_files/file1.yaml',
         'tests/test_data/yaml_files/file2.yaml'
         ) == data
+
+
+def test_complex_json():
+    with open('tests/test_data/complex_result.txt', 'r', encoding='utf-8') as file:
+        data = file.read()
+
+    assert generate_diff(
+        'tests/test_data/json_files/complex_file1.json',
+        'tests/test_data/json_files/complex_file2.json'
+    ) == data
+
+
+def test_complex_yaml():
+    with open('tests/test_data/complex_result.txt', 'r', encoding='utf-8') as file:
+        data = file.read()
+
+    assert generate_diff(
+        'tests/test_data/yaml_files/complex_file1.yaml',
+        'tests/test_data/yaml_files/complex_file2.yaml'
+    ) == data
