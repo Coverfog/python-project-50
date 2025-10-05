af8061525cbbbbd060a2378805f4251ad258fe5a
Coverfog <Hain.Heither@gmail.com>
2025-10-05 19:25:40 +0300

    Fix test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,10 +5,17 @@ def test_plain_json():
     with open('tests/test_data/result.txt', 'r', encoding='utf-8') as file:
         data = file.read()
     
-    assert generate_diff('tests/test_data/json_files/file1.json', 'tests/test_data/json_files/file2.json') == data
+    assert generate_diff(
+        'tests/test_data/json_files/file1.json',
+        'tests/test_data/json_files/file2.json'
+        ) == data
+
 
 def test_plain_yaml():
     with open('tests/test_data/result.txt', 'r', encoding='utf-8') as file:
         data = file.read()
     
-    assert generate_diff('tests/test_data/yaml_files/file1.yaml', 'tests/test_data/yaml_files/file2.yaml') == data
+    assert generate_diff(
+        'tests/test_data/yaml_files/file1.yaml',
+        'tests/test_data/yaml_files/file2.yaml'
+        ) == data
